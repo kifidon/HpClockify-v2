3550fbf6d23188da6c121ca247ebd2aabb02044e
kifidon <113075637+kifidon@users.noreply.github.com>
2024-03-27 15:18:08 -0600

    For Deployment testing with UI

--- Python/HpClockifyAPI.py
+++ Python/HpClockifyAPI.py
@@ -1,10 +1,12 @@
-import requests
-import pyodbc
-import pytz
-from datetime import datetime
 from fastapi import FastAPI, Request
 import logging
 import json
+import func
+import services
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
+
+#from fastapi.middleware.swagger import SwaggerUI
 
 # Configure logging
 logging.basicConfig(
@@ -13,117 +15,28 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'  # Set the log message format
 )
 
-def sqlConnect():
-    """
-    Connects to a SQL Server database.
-
-    Returns:
-        tuple: A tuple containing a cursor object and a connection object if the connection is successful, otherwise dict().
-    """
-    try:
-        #server info 
-        server = 'hpcs.database.windows.net'
-        database = 'hpdb'
-        username = 'hpUser'
-        password = '0153HP!!'
-        driver = '{ODBC Driver 18 for SQL Server}'
-        # Establish the connection
-        conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-        conn = pyodbc.connect(conn_str)
-        # Create a cursor object to interact with the database
-        cursor = conn.cursor()
-        return cursor, conn
-    except pyodbc.Error as e:
-        print(f"Error: {e}")
-        return None ,None
-
-def cleanUp(conn, cursor):
-    """
-    Closes the cursor and connection objects.
-
-    Args:
-        conn: The connection object to be closed.
-        cursor: The cursor object to be closed.
-
-    Returns:
-        int: 1 if the cleanup is successful, 0 otherwise.
-    """
-    try: 
-        cursor.close()
-        conn.close()
-        return "Completed"
-    except pyodbc.Error as e:
-        print(f"Error: {str(e)}")
-        return "Bad Clean UP"
-
-def timeZoneConvert(dateTime, format='%Y-%m-%dT%H:%M:%SZ'):
-    utcTimezone = pytz.utc
-    localTimeZone = pytz.timezone('America/Denver')
-    if isinstance(dateTime, str):
-        dateTime = datetime.strptime(dateTime, format)
-    output = utcTimezone.localize(dateTime).astimezone(localTimeZone)
-    return  output.replace(tzinfo=None)
-
-def timeDuration(duration_str):
-    """
-    Converts a duration string to hours.
-
-    Args:
-        duration_str (str): ISO 8601 duration format. In this format:
-                "PT" indicates a period of time.
-                "1H" indicates 1 hour.
-                "30M" indicates 30 minutes.
-            So, "PT1H30M" represents a duration of 1 hour and 30 minutes.
-
-    Returns:
-        float: Total duration in hours.
-    """
-    duration_str = duration_str[2:]
-    hours, minutes = 0, 0
-    if 'H' in duration_str:
-        hours, duration_str = duration_str.split('H')
-        hours = int(hours)
-    if 'M' in duration_str:
-        minutes = int( (duration_str.split('M'))[0] )/60
-    return( hours + minutes)
-
-def rowToJson(row):
-    json_data = {}
-    for column_name, value in zip(row.cursor_description, row):
-        # Extract column name
-        name = column_name[0]
-
-        # Convert to snake_case format
-        snake_case_name = ''.join(['_' + c.lower() if c.isupper() else c for c in name]).lstrip('_')
-
-        # Convert value to string and add to JSON dictionary
-        json_data[snake_case_name] = str(value)
-
-    return json_data
-
-def errorToJson(exc):
-    json_data = {
-        "error_class": str(exc.__class__),
-        "filename": exc.__traceback__.tb_frame.f_code.co_filename,
-        "function_name": exc.__traceback__.tb_frame.f_code.co_name,
-        "line_number": exc.__traceback__.tb_lineno,
-        "error_message": str(exc)
-    }
-    return json_data
 app = FastAPI()
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="Docs")
+
+@app.get("/openapi.json", include_in_schema=False)
+async def get_open_api_endpoint():
+    return JSONResponse(get_openapi(title="HpCLockifyAPI", version="1.0.0", routes=app.routes))
+
 @app.post('/timesheetUpdate')
-async def updateApproval(ApprovalR: Request):
+async def updateApproval(ApprovalR: services.ApprovalRequest):
     approve = await ApprovalR.json() # wait until request is sent 
     wkSpaceID = approve['workspaceId']
     aID = approve['id'] 
     userID = approve['owner']['userId']
     status = approve['status']['state']
     
-    startDateO = timeZoneConvert(approve['dateRange']['start'])
-    endDateO = timeZoneConvert(approve['dateRange']['end'])
+    startDateO = func.timeZoneConvert(approve['dateRange']['start'])
+    endDateO = func.timeZoneConvert(approve['dateRange']['end'])
     try:
-       cursor, conn =  sqlConnect() 
+       cursor, conn =  func.sqlConnect() 
        cursor.execute(
                 '''
                 UPDATE TimeSheet
@@ -144,9 +57,9 @@ async def updateApproval(ApprovalR: Request):
         )
         result = cursor.fetchone()
         conn.rollback()  # Roll back changes if an exception occurs
-        logging.error(f"An error occurred while updating timesheet: {json.dumps(errorToJson(exc), indent=4)}")
-        logging.error(f"{json.dumps(rowToJson(result), indent=4)}")
-        return errorToJson(exc)
+        logging.error(f"An error occurred while updating timesheet: {json.dumps(func.errorToJson(exc), indent=4)}")
+        logging.error(f"{json.dumps(func.rowToJson(result), indent=4)}")
+        return func.errorToJson(exc)
     else:
         cursor.execute(
            '''
@@ -157,7 +70,7 @@ async def updateApproval(ApprovalR: Request):
         result = cursor.fetchone()
         conn.commit() 
         logging.info("Committing changes...")  # Commit changes if no exceptions occurred                     
-        return(rowToJson(result))
+        return(func.rowToJson(result))
 
 
     
---
+++ Python/services.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Creator(BaseModel):
+    userId: str
+    userName: str
+    userEmail: str
+
+class Status(BaseModel):
+    state: str
+    updatedBy: str
+    updatedByUserName: str
+    updatedAt: str
+    note: Optional[str] = ""
+
+class Owner(BaseModel):
+    userId: str
+    userName: str
+    timeZone: str
+    startOfWeek: str
+
+class DateRange(BaseModel):
+    start: str
+    end: str
+
+class ApprovalRequest(BaseModel):
+    id: str
+    workspaceId: str
+    dateRange: DateRange
+    owner: Owner
+    status: Status
+    creator: Creator
+
+    # Specify example value for Swagger documentation
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "66034527a9e0d922a6783603",
+                "workspaceId": "65c249bfedeea53ae19d7dad",
+                "dateRange": {
+                    "start": "2024-03-17T06:00:00Z",
+                    "end": "2024-03-24T05:59:59Z"
+                },
+                "owner": {
+                    "userId": "65dcdd57ea15ab53ab7b14d7",
+                    "userName": "Margaretta Potts Petawaysin",
+                    "timeZone": "America/Denver",
+                    "startOfWeek": "SUNDAY"
+                },
+                "status": {
+                    "state": "APPROVED",
+                    "updatedBy": "65c253aeffbbb676c5e05ff2",
+                    "updatedByUserName": "Shawna Applejohn",
+                    "updatedAt": "2024-03-27T17:05:30Z",
+                    "note": ""
+                },
+                "creator": {
+                    "userId": "65dcdd57ea15ab53ab7b14d7",
+                    "userName": "Shawna Applejohn",
+                    "userEmail": "shawna.applejohn@hillplain.com"
+                }
+            }
+        }
