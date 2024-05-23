b8ef05efdd53042a9702d6930faa4c1cb8d2edb6
kifidon <113075637+kifidon@users.noreply.github.com>
2024-05-23 13:56:06 -0600

    minor bug fixes

--- HpApiClockify/HpClockfiyApi/HpClockfiyApi/serializers.py
+++ HpApiClockify/HpClockfiyApi/HpClockfiyApi/serializers.py
@@ -14,6 +14,7 @@ from .models import(
 from .clockify_util.hpUtil import count_working_daysV2, timeZoneConvert, timeDuration, get_current_time
 from json import dumps
 from datetime import date
+
 class EmployeeUserSerializer(serializers.ModelSerializer):
     '''
     Serialized to the EmployeeUser Model fields. 
@@ -98,7 +99,7 @@ class EmployeeUserSerializer(serializers.ModelSerializer):
         try:
             return field['Start Date']
         except Exception: 
-            return date.today()
+            return date.today().strftime('%Y-%m-%d')
     
     def create(self, validated_data):
         logger = setup_background_logger() 
