53737c2624b22ba889503fbd1441e0b73d757c5e
kifidon <113075637+kifidon@users.noreply.github.com>
2024-10-15 15:13:38 -0600

    Debug

--- HpApiClockify/HpClockfiyApi/HpClockfiyApi/clockify_util/QuickBackupV3.py
+++ HpApiClockify/HpClockfiyApi/HpClockfiyApi/clockify_util/QuickBackupV3.py
@@ -219,8 +219,8 @@ async def billingReport(month= None, year = None, pCode = None, start=None, end 
             previousYear = year
 
     #format date strings
-        endDateObj = datetime.strptime(f'20{year}-{month}-25', '%Y-%m-%d')
-        startDateObj = datetime.strptime(f'20{previousYear}-{previousMonth}-25', '%Y-%m-%d')
+        endDateObj = datetime.datetime.strptime(f'20{year}-{month}-25', '%Y-%m-%d')
+        startDateObj = datetime.datetime.strptime(f'20{previousYear}-{previousMonth}-25', '%Y-%m-%d')
         # Calculate the most recent previous Saturday
         if(endDateObj.weekday() != 5):
             endDate = (endDateObj - timedelta(days=(endDateObj.weekday() + 2) % 7)).strftime('%Y-%m-%d')
