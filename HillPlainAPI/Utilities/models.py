d644cf41e00b0732bc69408e6f47ba12c6d6c1f6
kifidon <113075637+kifidon@users.noreply.github.com>
2024-11-12 10:17:09 -0700

    Re submit

--- HillPlainAPI/Utilities/models.py
+++ HillPlainAPI/Utilities/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class BackGroundTaskResult(models.Model):
+    status_code = models.IntegerField(default=404)
+    message = models.TextField(blank=True, null= True)
+    data = models.TextField(blank=False, null=False )
+    time = models.DateTimeField(blank=False, null=False)
+    caller = models.CharField(max_length=50 )
+    class Meta:
+        managed = False
+        db_table = 'BackGroundTaskDjango'
+
 # Create your models here.
