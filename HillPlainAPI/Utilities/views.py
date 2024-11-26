1c1da8cba8e38962f627fd6e9028408587dac389
kifidon <113075637+kifidon@users.noreply.github.com>
2024-11-26 16:21:43 -0700

    Saving state for push before test Cases

--- HillPlainAPI/Utilities/views.py
+++ HillPlainAPI/Utilities/views.py
@@ -173,7 +173,7 @@ def taskResult(response: JsonResponse, inputData, caller: str):
         message = response.content.decode() or None,
         data = inputData,
         caller = caller,
-        time = time.time()
+        time = str(time.time())
     )
 
 '''
